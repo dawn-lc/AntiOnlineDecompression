a4a45c2d7f715aafc6dea79d5e1da26f533efa8f
dawn-lc <lc990930@163.com>
2023-07-17 11:16:54 +0800

    添加项目文件。

---
+++ ExtensionMethods.cs
@@ -0,0 +1,183 @@
+﻿using CSChaCha20;
+using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AntiOnlineDecompression
+{
+    public static class ExtensionMethods
+    {
+        private static readonly SHA256 SHA256 = SHA256.Create();
+        /// <summary>
+        /// 计算SHA256Hash
+        /// </summary>
+        /// <param name="inputStream">数据流</param>
+        /// <returns>SHA256Hash字节数组</returns>
+        public static async Task<byte[]> SHA256HashAsync(this Stream inputStream)
+        {
+            long save = inputStream.Position;
+            byte[] SHA256Hash = await SHA256.ComputeHashAsync(inputStream);
+            inputStream.Seek(save, SeekOrigin.Begin);
+            return SHA256Hash;
+        }
+        /// <summary>
+        /// 计算SHA256Hash
+        /// </summary>
+        /// <param name="inputBytes">数据</param>
+        /// <returns>SHA256Hash字节数组</returns>
+        public static byte[] SHA256Hash(this byte[] inputBytes)
+        {
+            return SHA256.ComputeHash(inputBytes);
+        }
+        /// <summary>
+        /// 字节数组转16进制字符串
+        /// </summary>
+        /// <param name="byteDatas"></param>
+        /// <returns></returns>
+        public static string BytesToHexString(this byte[] byteDatas)
+        {
+            StringBuilder builder = new();
+            for (int i = 0; i < byteDatas.Length; i++)
+            {
+                builder.Append(string.Format("{0:X2}", byteDatas[i]));
+            }
+            return builder.ToString();
+        }
+        /// <summary>
+        /// 16进制字符串转字节数组
+        /// </summary>
+        /// <param name="HexString"></param>
+        /// <returns></returns>
+        public static byte[] HexStringToBytes(this string HexString)
+        {
+            if (HexString.Length % 2 != 0) throw new ArgumentException("Format err", nameof(HexString));
+            char[] Hex = HexString.ToCharArray();
+            byte[] bytes = new byte[Hex.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte($"{Hex[i * 2]}{Hex[(i * 2) + 1]}", 16);
+            }
+            return bytes;
+        }
+        /// <summary>
+        /// 字节数组是否相等
+        /// </summary>
+        /// <param name="byteDatas"></param>
+        /// <returns></returns>
+        public static bool SequenceCompare(this byte[]? a, byte[]? b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (a is null || b is null) return false;
+            if (a.Length != b.Length) return false;
+            return a.SequenceEqual(b);
+        }
+
+        /// <summary>
+        /// byte[] 不同计数
+        /// </summary>
+        /// <param name="byteDatas"></param>
+        /// <returns></returns>
+        public static int Differences(this byte[] a, byte[] b)
+        {
+            return new BitArray(a).Differences(new BitArray(b));
+        }
+        /// <summary>
+        /// BitArray 不同计数
+        /// </summary>
+        /// <param name="byteDatas"></param>
+        /// <returns></returns>
+        public static int Differences(this BitArray a, BitArray b)
+        {
+            int differences = 0;
+            BitArray xor = a.Xor(b);
+            for (int i = 0; i < xor.Length; i++)
+            {
+                if (xor[i])
+                {
+                    differences++;
+                }
+            }
+            return differences;
+        }
+        public static byte[] RandomBytes(int length)
+        {
+            return RandomNumberGenerator.GetBytes(length);
+        }
+        public static byte[] StreamRead(this Stream stream, long a, long b)
+        {
+            if ((b - a) > int.MaxValue)
+            {
+                throw new OverflowException();
+            }
+            long save = stream.Position;
+            int DataLength = (int)(b - a);
+            byte[] ReadData = new byte[DataLength];
+            stream.Seek(a, SeekOrigin.Begin);
+            stream.Read(ReadData, 0, DataLength);
+            stream.Seek(save, SeekOrigin.Begin);
+            return ReadData;
+        }
+        public static byte[] BytesRead(this byte[] bytes, ref int pos, int length)
+        {
+            int p = pos;
+            pos += length;
+            return bytes[p..(p+length)];
+        }
+        
+        public static byte[] StringToBytes(string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+        public static byte[] IntToBytes(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+        public static string BytesToString(this byte[] value)
+        {
+            return Encoding.UTF8.GetString(value);
+        }
+        public static int BytesToInt(this byte[] value)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(value);
+            }
+            return BitConverter.ToInt32(value);
+        }
+        
+        public static async Task Encrypto(this Stream data, Stream encrypted, byte[] key, byte[] iv)
+        { 
+            int bytesRead;
+            byte[] buffer = new byte[102400000];
+            ChaCha20 Encrypto = new(key, iv, 0);
+            while ((bytesRead = await data.ReadAsync(buffer)) != 0)
+            {
+                byte[] encryptedBuffer = new byte[bytesRead];
+                Encrypto.EncryptBytes(encryptedBuffer, buffer.AsMemory(0, bytesRead).ToArray());
+                await encrypted.WriteAsync(encryptedBuffer);
+            }
+        }
+        public static async Task Decrypto(this Stream encrypted, Stream data, byte[] key, byte[] iv)
+        {
+            int bytesRead;
+            byte[] buffer = new byte[102400000];
+            ChaCha20 Decrypto = new(key, iv, 0);
+            while ((bytesRead = await encrypted.ReadAsync(buffer)) != 0)
+            {
+                byte[] decryptedBuffer = new byte[bytesRead];
+                Decrypto.DecryptBytes(decryptedBuffer, buffer.AsMemory(0, bytesRead).ToArray());
+                await data.WriteAsync(decryptedBuffer);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using static AntiOnlineDecompression.ExtensionMethods;
+
+namespace AntiOnlineDecompression
+{
+    class Program
+    {
+        interface IBinaryObject
+        {
+            public byte[] Serialize();
+        }
+        public static class Crypto
+        {
+            public class CryptoHead : IBinaryObject
+            {
+                public required byte[] IV { get; set; }
+                public required byte[] Hash { get; set; }
+                public byte[] Serialize()
+                {
+                    List<byte> data = new();
+                    data.AddRange(IntToBytes(IV.Length));
+                    data.AddRange(IV);
+                    data.AddRange(IntToBytes(Hash.Length));
+                    data.AddRange(Hash);
+                    return data.ToArray();
+                }
+
+                internal static CryptoHead Deserialize(byte[] bytes)
+                {
+                    int pos = 0;
+                    return new CryptoHead()
+                    {
+                        IV = bytes.BytesRead(ref pos, bytes.BytesRead(ref pos, 4).BytesToInt()),
+                        Hash = bytes.BytesRead(ref pos, bytes.BytesRead(ref pos, 4).BytesToInt())
+                    };
+                }
+            }
+            public class CryptoKey : IBinaryObject
+            {
+                public required byte[] Key { get; set; }
+                public required byte[] Hash { get; set; }
+                public required string Name { get; set; }
+                public byte[] Serialize()
+                {
+                    List<byte> data = new();
+                    data.AddRange(IntToBytes(Key.Length));
+                    data.AddRange(Key);
+                    data.AddRange(IntToBytes(Hash.Length));
+                    data.AddRange(Hash);
+                    data.AddRange(IntToBytes(StringToBytes(Name).Length));
+                    data.AddRange(StringToBytes(Name));
+                    return data.ToArray();
+                }
+                internal static CryptoKey Deserialize(byte[] bytes)
+                {
+                    int pos = 0;
+                    return new()
+                    {
+                        Key = bytes.BytesRead(ref pos, bytes.BytesRead(ref pos, 4).BytesToInt()),
+                        Hash = bytes.BytesRead(ref pos, bytes.BytesRead(ref pos, 4).BytesToInt()),
+                        Name = bytes.BytesRead(ref pos, bytes.BytesRead(ref pos, 4).BytesToInt()).BytesToString()
+                };
+                }
+            }
+            public static async Task Encrypto(Stream data, Stream cryptodata, byte[] key)
+            {
+                long SaveDataPosition = data.Position;
+                cryptodata.SetLength(0);
+
+                CryptoHead Head = new()
+                {
+                    IV = RandomBytes(12),
+                    Hash = await data.SHA256HashAsync()
+                };
+                byte[] HeadBytes = Head.Serialize();
+                await cryptodata.WriteAsync(StringToBytes("AODF"));
+                await cryptodata.WriteAsync(IntToBytes(HeadBytes.Length));
+                await cryptodata.WriteAsync(HeadBytes);
+
+                await data.Encrypto(cryptodata, key, Head.IV);
+
+                data.Seek(SaveDataPosition, SeekOrigin.Begin);
+                cryptodata.Seek(0, SeekOrigin.Begin);
+            }
+            public static async Task Decrypto(Stream cryptodata, Stream data, byte[] key)
+            {
+                long SaveCryptoDataPosition = cryptodata.Position;
+                data.SetLength(0);
+
+                int HeadLength = cryptodata.StreamRead(4, 8).BytesToInt();
+                CryptoHead FileHead = CryptoHead.Deserialize(cryptodata.StreamRead(8, 8 + HeadLength));
+                cryptodata.Seek(8 + HeadLength, SeekOrigin.Begin);
+                await cryptodata.Decrypto(data, key, FileHead.IV);
+                data.Seek(0, SeekOrigin.Begin);
+                if (!(await data.SHA256HashAsync()).SequenceCompare(FileHead.Hash))
+                {
+                    throw new Exception("校验失败！");
+                }
+
+                cryptodata.Seek(SaveCryptoDataPosition, SeekOrigin.Begin);
+                data.Seek(0, SeekOrigin.Begin);
+            }
+        }
+
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                FileInfo? file;
+                if (!args.Any())
+                {
+                    IEnumerable<FileInfo> fileInfos = new DirectoryInfo(Environment.CurrentDirectory).GetFiles().Where(i => i.FullName != Environment.ProcessPath);
+                    file = fileInfos.FirstOrDefault(i =>
+                    {
+                        FileStream? stream = null;
+                        try
+                        {
+                            stream = i.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+                            return stream.StreamRead(0, 4).BytesToString() == "AODF";
+                        }
+                        catch (Exception)
+                        {
+                            return false;
+                        }
+                        finally
+                        {
+                            stream?.Dispose();
+                        }
+                    });
+                    if (!fileInfos.Any() || fileInfos.Count() < 2 || file == null)
+                    {
+                        throw new FileNotFoundException($"待解密文件不存在或无法访问！");
+                    }
+                }
+                else
+                {
+                    file = new FileInfo(args[0].Replace("\"", ""));
+                }
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException(args[0]);
+                }
+                Crypto.CryptoKey cryptoKey;
+
+                using FileStream InputFile = file.Open(FileMode.Open, FileAccess.Read);
+                if (InputFile.StreamRead(0, 4).BytesToString() != "AODF")
+                {
+                    byte[] Key = RandomBytes(32);
+                    string EncryptoFileName;
+                    EncryptoFileName = (await InputFile.SHA256HashAsync()).BytesToHexString();
+                    using FileStream EncryptFile = new($"{EncryptoFileName}", FileMode.OpenOrCreate);
+                    await Crypto.Encrypto(InputFile, EncryptFile, Key);
+                    cryptoKey = new()
+                    {
+                        Key = Key,
+                        Hash = await EncryptFile.SHA256HashAsync(),
+                        Name = file.Name
+                    };
+                    using (FileStream KeyFile = new($"{EncryptoFileName}.key", FileMode.Create))
+                    {
+                        KeyFile.SetLength(0);
+                        await KeyFile.WriteAsync(cryptoKey.Serialize());
+                    }
+                    Console.WriteLine("加密完成。");
+                    return 0;
+                }
+                else
+                {
+                    if (!File.Exists($"{file.FullName}.key"))
+                    {
+                        throw new FileNotFoundException($"解密密钥 {file.FullName}.key 不存在或无法访问！");
+                    }
+                    using (FileStream KeyFile = new($"{file.FullName}.key", FileMode.Open))
+                    {
+                        cryptoKey = Crypto.CryptoKey.Deserialize(KeyFile.StreamRead(0, KeyFile.Length));
+                    }
+                    byte[] EncryptFileHash = await InputFile.SHA256HashAsync();
+                    if (!EncryptFileHash.SequenceCompare(cryptoKey.Hash))
+                    {
+                        throw new Exception($"加密文件校验失败！SHA256:{EncryptFileHash.BytesToHexString()}");
+                    }
+                    using FileStream DecryptFile = new(cryptoKey.Name, FileMode.Create);
+                    await Crypto.Decrypto(InputFile, DecryptFile, cryptoKey.Key);
+                    Console.WriteLine("解密完成。");
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return 1;
+            }
+        }
+    }
+}
